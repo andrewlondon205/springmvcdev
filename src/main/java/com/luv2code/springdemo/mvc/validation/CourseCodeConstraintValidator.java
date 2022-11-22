5f7895739a81b25e953679484364ea50379492ec
AlejandriaRamai <vyasadev931@gmail.com>
2022-11-22 17:59:57 +0100

    further updates

---
+++ src/main/java/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode,String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext context) {
+        boolean result = theCode.startsWith(coursePrefix);
+        return result;
+    }
+}
